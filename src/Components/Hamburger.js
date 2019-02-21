d35201e0e0e007805f0b4390e3995c42ce2d977a
Nicola Lombardi <nicolalombardi.dev@gmail.com>
2019-02-21 23:25:53 +0100

    added avatar to hamburger

--- src/Components/Hamburger.js
+++ src/Components/Hamburger.js
@@ -1,9 +1,35 @@
 import React from "react"
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer"
+import Avatar from "@material-ui/core/Avatar"
+import Typography from "@material-ui/core/Typography"
 
 const Hamburger = props => (
   <SwipeableDrawer onOpen={() => {}} open={props.open} onClose={props.close}>
-    <div style={{ maxWidth: "360px", minWidth: "360px" }}>a</div>
+    <div
+      style={{
+        maxWidth: "360px",
+        width: "80vw",
+        backgroundColor: "#e0e0e0",
+        height: "100%"
+      }}
+    >
+      <Avatar
+        style={{
+          marginTop: "64px",
+          marginLeft: "64px",
+          marginRight: "64px",
+          maxWidth: "232px",
+          width: "calc(100% - 128px)",
+          maxHeight: "232px",
+          height: "calc(80vw - 128px)",
+          backgroundColor: "#ff0000"
+        }}
+      >
+        <Typography style={{ color: "white" }} variant="h1">
+          NL
+        </Typography>
+      </Avatar>
+    </div>
   </SwipeableDrawer>
 )
 
